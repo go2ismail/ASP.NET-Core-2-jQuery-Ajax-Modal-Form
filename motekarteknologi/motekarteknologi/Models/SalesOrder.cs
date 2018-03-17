8906c49080d1b02d490bc696db349d7544cbe06b
ismail hamzah <gohamzah@outlook.com>
2018-03-17 09:38:01 +0700

    creating models: salesorder, salesorderline and product

---
+++ motekarteknologi/motekarteknologi/Models/SalesOrder.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace motekarteknologi.Models
+{
+    public class SalesOrder
+    {
+        public Guid ID { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Name { get; set; }
+        [StringLength(200)]
+        public string Description { get; set; }
+
+        public ICollection<SalesOrderLine> Lines { get; set; } = new List<SalesOrderLine>();
+    }
+}
---
+++ motekarteknologi/motekarteknologi/Models/SalesOrderLine.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace motekarteknologi.Models
+{
+    public class SalesOrderLine
+    {
+        public Guid ID { get; set; }
+        
+        public Guid SalesOrderID { get; set; }
+        public SalesOrder SalesOrder { get; set; }
+
+        public Guid ProductID { get; set; }
+        public Product Product { get; set; }
+        [StringLength(200)]
+        public string Description { get; set; }
+        public int Qty { get; set; }
+    }
+}
